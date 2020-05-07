a10ec9eceb6a8458109e714db0ecdf16ae40d705
Faisal Rashid <FRashid@dealersocket.com>
2020-05-08 00:36:08 +0530

    Updates

---
+++ precache-manifest.f77b173263077d3ee349e41ad09df90b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f889b0bdafb2640b6fa6a07ebbb42b1f",
+    "url": "/fullpage-react-fs/index.html"
+  },
+  {
+    "revision": "171194c03506f76f7e43",
+    "url": "/fullpage-react-fs/static/css/main.b59db21a.chunk.css"
+  },
+  {
+    "revision": "1ce50dbb15d85166c6f6",
+    "url": "/fullpage-react-fs/static/js/2.9bb2a4bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fullpage-react-fs/static/js/2.9bb2a4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171194c03506f76f7e43",
+    "url": "/fullpage-react-fs/static/js/main.b8ae1aa5.chunk.js"
+  },
+  {
+    "revision": "c9111847892cf4b62fa7",
+    "url": "/fullpage-react-fs/static/js/runtime-main.3767435c.js"
+  }
+]);
